ee573c844063604ffab1c1550f282c1ceaf625df
Your Name <your.email@example.com>
2024-08-03 21:12:24 +0530

    Modifications 03-08-24 by manishkip

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-var typed= new Typed(".multiple-text",{
-    strings: ["Software Engineer","Web Developer "],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay: 1000,
-    loop:true
-})
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if the current page is index.html
+    if (window.location.pathname.includes("index.html")) {
+        // Initialize the Typed instance only if on index.html
+        var typed = new Typed(".multiple-text", {
+            strings: ["Software Engineer", "Web Developer"],
+            typeSpeed: 100,
+            backSpeed: 100,
+            backDelay: 1000,
+            loop: true
+        });
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById("menu-icon");
+    const menu = document.getElementById("menu");
+
+    menuIcon.addEventListener("click", function() {
+        menu.classList.toggle("active");
+    });
+});
+
